0a46e3d1c7536fef4dd2bedfecd037a1b2d0335f
Mohammad Apon <umar426.net@gmail.com>
2023-06-20 22:48:58 +0600

    alhamdulillah done push

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -3,10 +3,10 @@ import logo from '../../../assets/image 32.png'
 const Navbar = () => {
 
     const navLink = <>
-        <li><a className='font-semibold hover:text-blue-500 divide-x'>Home</a></li>
-        <li><a>About</a></li>
-        <li><a>Course</a></li>
-        <li><a>Contact</a></li>
+        <li><a className='font-semibold hover:text-blue-500 hover:border-blue-500 border-transparent cursor-pointer border-b-2 w-1/2'>Home</a></li>
+        <li><a className='font-semibold hover:text-blue-500 hover:border-blue-500 border-transparent cursor-pointer border-b-2 w-1/2'>About</a></li>
+        <li><a className='font-semibold hover:text-blue-500 hover:border-blue-500 border-transparent cursor-pointer border-b-2 w-1/2'>Course</a></li>
+        <li><a className='font-semibold hover:text-blue-500 hover:border-blue-500 border-transparent cursor-pointer border-b-2 w-1/2'>Contact</a></li>
     </>
     return (
         <div className="navbar bg-slate-100-100 lg:px-24 px-6">
@@ -15,7 +15,7 @@ const Navbar = () => {
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 z-10">
 
                         {navLink}
 
@@ -24,7 +24,7 @@ const Navbar = () => {
                 <img className='lg:w-32 w-20' src={logo} alt="" />
             </div>
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
+                <ul className="flex items-center justify-center gap-10">
 
                     {navLink}
 
